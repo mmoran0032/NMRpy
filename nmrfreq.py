01d3f094d5126b6d64bd85760c2ce0c52e4faa49
mmoran0032 <mmoran0032@gmail.com>
2015-03-26 02:44:02 -0400

    Moved version location

--- nmrfreq.py
+++ nmrfreq.py
@@ -2,6 +2,7 @@
 # nmrfreq - NMR frequency calculator
 
 
+# Version numbering follows Semantic Versioning (http://semver.org/)
 versionNum = "0.9.99"
 versionDate = "2015-XX-XX"
 
--- share/config.py
+++ share/config.py
@@ -7,8 +7,3 @@ magnetK = 129.88
 magnetKerror = 0.05
 
 outputDirectory = "~"
-
-# Internal values
-# Version numbering follows Semantic Versioning (http://semver.org/)
-versionNumber = "0.9.99"
-versionDate = "2015-XX-XX"
