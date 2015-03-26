141f5773b7710d1723b519a6ec96b8e41005147c
mmoran0032 <mmoran0032@gmail.com>
2015-03-25 23:37:32 -0400

    Isotope class "finished"
    For now, Isotope.py looks to be done. processName() will need some more work probably, but I can't think of a better way to do things now.

--- include/Isotope.py
+++ include/Isotope.py
@@ -5,21 +5,26 @@ class Isotope(object):
   """
   From desired isotope name (XXX### or similar), we get values for the
   charge state and mass of that isotope from our desired mass table
-  dictionary. Name is checked before we try to get the information
+  dictionary. Name is checked before we try to get the information.
+
+  In all cases, if things fail, we default back to a basic definition for
+  Hydrogen-1, while informing the user that we're doing that.
   """
-  def __init__(self, name, masstable={"H1": (1, 1)}):
+  def __init__(self, name, masstable={"H1": (1, 1.0)}, show=True):
     """
     Creates the isotope. masstable defaults to dummy Hydrogen-1 if no table
     provided to make sure we have some information after starting.
     """
     self.name = name
-    self.mass = 0.0
+    self.mass = 0
     self.Z = 0
-    self.reformatName()
-    self.fillValues(masstable)
+    self.masstable = masstable
+    self.processName()
+    if show:
+      self.showIsotope()
 
 
-  def reformatName(self):
+  def processName(self):
     from re import findall
     iso = self.name.upper()
     # splits name into symbol and number
@@ -34,23 +39,29 @@ class Isotope(object):
     else:
       print("Isotope {0} not valid, defaulting to H1".format(self.name))
       self.changeName()
+      self.processName()
+    self.fillValues()
 
 
-  def fillValues(self, masstable):
+  def fillValues(self):
     """
     Takes values from preprocessed masstable (dictionary) and saves them.
     Requires to already have checked and reformated the isotope.
     """
-    if self.name in masstable:
-      self.mass = masstable[self.name][1]
-      self.Z = masstable[self.name][0]
+    if self.name in self.masstable:
+      self.mass = self.masstable[self.name][1]
+      self.Z = self.masstable[self.name][0]
     else:
       print("Isotope {0} not found in masstable".format(self.name))
 
 
+  def showIsotope(self):
+    print("Isotope: {0.name} (Z = {0.Z}, mass = {0.mass} amu)".format(self))
+
+
   def changeName(self, newName="H1"):
     self.name = newName
-    self.reformatName()
+    self.processName()
 
 
   def _createName(self, symbol, number):
@@ -61,8 +72,9 @@ def main():
   # Testing with different isotope names
   i = Isotope("he4")
   i.changeName("97ZR")
+  i.showIsotope()
   i.changeName("INVALID-NAME-1")
-  i.changeName("12-C")
+  i.showIsotope()
 
 
 if __name__ == "__main__":
