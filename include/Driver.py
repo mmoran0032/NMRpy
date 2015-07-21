742f377e13e4aa4a03d2c11a9083ba6394466fc2
Mike Moran <mmoran0032@gmail.com>
2015-07-21 18:36:38 -0400

    Beginning the transition to including ranges

--- include/Driver.py
+++ include/Driver.py
@@ -22,10 +22,10 @@ class Driver(object):
                    version="nmrfreq {0}".format(self.version))
     p.add_argument("-i", "--isotope", type=str, default="H1",
                    help="desired isotope name")
-    p.add_argument("-q", "--charge", type=int, default=1,
+    p.add_argument("-q", "--charge", type=str, default=1,
                    help="selected charge state")
     g = p.add_mutually_exclusive_group()
-    g.add_argument("-e", "--energy", type=float,
+    g.add_argument("-e", "--energy", type=str,
                    help="desired beam energy")
     g.add_argument("-f", "--frequency", type=float, metavar="FREQ",
                    help="desired NMR frequency")
@@ -42,6 +42,12 @@ class Driver(object):
     self.isotope = Isotope(self.isotope, self.masstable)
 
   def performCalculation(self):
-    self.calc = NMRcalc(self.isotope, self.config, self.charge,
-                        self.energy, self.frequency)
+    charge, energy = self.convertValues(self.charge, self.energy)
+    self.calc = NMRcalc(self.isotope, self.config, charge,
+                        energy, self.frequency)
     self.calc.processValues()
+
+  def convertValues(self, charge, energy):
+    charge = int(charge)
+    energy = float(energy)
+    return charge, energy
